547486722834a700078b6a5627a754a161bd9401
Shane Kaestner <shane.kaestner@yahoo.com>
2020-04-21 00:06:59 -0700

    Started working on a binary search for the number guesser so the user can enter any number and have the computer eventually use an array to figure out the number if the user cannot.

--- randomePython.py
+++ randomePython.py
@@ -1,25 +1,35 @@
-# This is a guess the number game.
+# This is a guesses the number game.
 
-import random
+import random, math
 
 print("Hello what is your name?")
 name = input()
 
-print("Well, " + ", I am thinking of a number between 1 and 20")
+print("Well, " + ", I am thinking of a number between 1 and a number of your choosing")
+print("Choose the max possible number'")
+numberMax = int(input())
 
-secretNumber = random.randint(1, 20)
-
-for guessesTaken in range(1,7):
-    print('Take a guess.')
-    guess = int(input())
-    if(guess < secretNumber):
-        print('Your guess is too low')
-    elif(guess > secretNumber):
-        print('Your guess is too high')
+#print('DEBUG: secret number is ' + str(secretNumber))
+def numberGuesser(num):
+    secretNumber = random.randint(1, num)
+    for guessesTaken in range(1,7):
+        print('Take a guess.')
+        guess = int(input())
+        if(guess < secretNumber):
+            print('Your guess is too low')
+        elif(guess > secretNumber):
+            print('Your guess is too high')
+        else:
+            break #the condition was satisfied
+    if(guess == secretNumber):
+        print('Congratulations '+ name +' you got it in ' + str(guessesTaken) + ' guesses')
     else:
-        break #the condition was satisfied
+        print('Nope the number I was thinking was ' + str(secretNumber))
+print("I'm sure that's a fantastic number")
+maxAttempts = math.ceil(math.log(numberMax, 2))
+print("Technically it should only take you at most "+ str(maxAttempts) +" to figure out the random number I come up with when given "+ str(numberMax))
+print("Although I don't envy your task if you choose to take on that endeavor")
+numberGuesser(numberMax)
 
-if(guess == secretNumber):
-    print('Congratulations you got it in ' + str(guessesTaken) + ' guesses')
-else:
-    print('Nope the nmber I was thinking was ' + str(secretNumber))
+
+#import math /// math.log(a,Base)
